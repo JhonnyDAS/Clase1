e2fae911e2a2bec82a286a4f4c145b0a6accf86b
Jhonny <jhonnla27@gmail.com>
2018-02-26 19:44:53 -0400

    Primera Clase programacion Basica

---
+++ Clase1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Clase1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumUno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumDos = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumUno
+            // 
+            this.txtNumUno.Location = new System.Drawing.Point(105, 32);
+            this.txtNumUno.Name = "txtNumUno";
+            this.txtNumUno.Size = new System.Drawing.Size(100, 20);
+            this.txtNumUno.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Primer Numero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Segundo Numero";
+            // 
+            // txtNumDos
+            // 
+            this.txtNumDos.Location = new System.Drawing.Point(105, 58);
+            this.txtNumDos.Name = "txtNumDos";
+            this.txtNumDos.Size = new System.Drawing.Size(100, 20);
+            this.txtNumDos.TabIndex = 2;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(102, 81);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(35, 13);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "label3";
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(130, 97);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.TabIndex = 5;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 138);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumDos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumUno);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumUno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumDos;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSumar;
+    }
+}
+
---
+++ Clase1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clase1
+{
+    public partial class Form1 : Form
+    {
+        double _a, _b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            Validar(out a, out b);
+            Resultado(a, b);
+        }
+
+        private void Resultado(double a, double b)
+        {
+            double s;
+            s = a + b;
+            lblResultado.Text = s.ToString();
+        }
+
+        private void Validar(out double a, out double b)
+        {
+            if (txtNumUno.Text != "")
+                a = Convert.ToDouble(txtNumUno.Text);
+            else
+                a = 0;
+
+            if (txtNumDos.Text != String.Empty)
+                b = Convert.ToDouble(txtNumDos.Text);
+            else
+                b = 0;
+        }
+    }
+}
